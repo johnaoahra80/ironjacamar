aff04ddacdd9d60c17d6bbc1361784a76ac00042
John O'Hara <johnaohara80@gmail.com>
2014-07-04 07:45:49 +0100

    Fixed idleRemover bug

--- core/src/main/java/org/jboss/jca/core/connectionmanager/pool/mcp/SemaphoreConcurrentLinkedQueueManagedConnectionPool.java
+++ core/src/main/java/org/jboss/jca/core/connectionmanager/pool/mcp/SemaphoreConcurrentLinkedQueueManagedConnectionPool.java
@@ -934,7 +934,7 @@ public class SemaphoreConcurrentLinkedQueueManagedConnectionPool implements Mana
          ConnectionListenerWrapper clw = clwIter.next();
 
          destroy = decrementer.shouldDestroy(clw.getConnectionListener(),
-               timeout, cls.size() + checkedOutSize.get(),
+               timeout, poolSize.get(),
                poolConfiguration.getMinSize(), destroyed);
 
          if (destroy) 
